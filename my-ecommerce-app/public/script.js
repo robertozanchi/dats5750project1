358fede3d510e9d2d8336c1055a83ba0861cea90
Roberto Zanchi <robertozanchi@Robertos-Air.lan>
2023-08-31 07:07:01 -0400

    Refactored JS

--- my-ecommerce-app/public/script.js
+++ my-ecommerce-app/public/script.js
@@ -1,81 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const addToCartButtons = document.querySelectorAll('.add-to-cart');
   const cartItemsList = document.getElementById('cart-items');
-  const checkoutButton = document.getElementById('checkout-btn'); // Get the "Checkout" button element
+  const checkoutButton = document.getElementById('checkout-btn');
 
-  addToCartButtons.forEach(button => {
-    button.addEventListener('click', async event => {
+  document.body.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('add-to-cart')) {
       const productId = event.target.getAttribute('data-product-id');
-      const response = await addToCart(productId);
-      
-  updateCartUI(response.cart);
-  updateOrderTotal(response.cartTotal);
-
-    });
+      const response = await performFetch('/api/add-to-cart', { productId });
+      updateCartUI(response.cart);
+      updateOrderTotal(response.cartTotal);
+    }
   });
 
-  // Add event listener for the "Checkout" button
   checkoutButton.addEventListener('click', async () => {
-    const response = await checkout();
-    // Update UI after checkout response
-    
-  updateCartUI(response.cart);
-  updateOrderTotal(response.cartTotal);
-
-    updateOrderTotal(response.orderTotal); // Update the displayed order total
+    const response = await performFetch('/api/checkout');
+    updateCartUI(response.cart);
+    updateOrderTotal(response.orderTotal);
   });
 
-  async function addToCart(productId) {
-    const response = await fetch('/api/add-to-cart', {
+  async function performFetch(url, body = {}) {
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ productId }),
+      body: JSON.stringify(body),
     });
     return response.json();
   }
 
-  async function checkout() {
-    const response = await fetch('/api/checkout', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({}),
-    });
-    const data = await response.json();
-  
-    // Update the UI with the order total
-    const orderTotalElement = document.getElementById('order-total');
-    orderTotalElement.textContent = `Order Total: $${data.orderTotal}`;
-  
-    return data;
-  }
-
   function updateCartUI(cartItems) {
     cartItemsList.innerHTML = '';
+    const fragment = document.createDocumentFragment();
     cartItems.forEach(item => {
       const li = document.createElement('li');
       li.textContent = `${getProductName(item.id)} - $${item.price}`;
-      cartItemsList.appendChild(li);
+      fragment.appendChild(li);
     });
+    cartItemsList.appendChild(fragment);
   }
-  
-  // Function to get the product name based on the product id
-function getProductName(productId) {
-  if (productId === '1') {
-    return 'Comforter'; // Replace with your desired product name
-  } else if (productId === '2') {
-    return 'Cover'; // Replace with your desired product name
-  } else {
-    return 'Unknown Product'; // Default product name if productId is not recognized
-  }
-}
 
-// Update the order total in the UI
-function updateOrderTotal(total) {
-  const orderTotalElement = document.getElementById('order-total');
-  orderTotalElement.textContent = `Order Total: $${total}`;
-}
+  function getProductName(productId) {
+    const productNames = {
+      '1': 'Comforter',
+      '2': 'Cover'
+    };
+    return productNames[productId] || 'Unknown Product';
+  }
+
+  function updateOrderTotal(total) {
+    const orderTotalElement = document.getElementById('order-total');
+    orderTotalElement.textContent = `Order Total: $${total}`;
+  }
 });
--- my-ecommerce-app/server.js
+++ my-ecommerce-app/server.js
@@ -2,49 +2,34 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const cart = []; // Cart array to store added items
+const cart = [];
 
 app.use(express.static('public'));
 app.use(express.json());
 
+const PRODUCT_INFO = {
+  '1': { name: 'Cotton Comforter', price: 150 },
+  '2': { name: 'Linen Cover', price: 200 }
+};
+
 app.post('/api/add-to-cart', (req, res) => {
   const { productId } = req.body;
+
+  if (!PRODUCT_INFO[productId]) {
+    return res.status(400).json({ message: 'Invalid product ID' });
+  }
+
   const product = {
     id: productId,
-    name: getProductName(productId), // Use the function to get the product name
-    price: getProductPrice(productId)
+    ...PRODUCT_INFO[productId]
   };
-  cart.push(product);
-  
-  const cartTotal = calculateCartTotal();
-  res.json({ message: 'Product added to cart', cart, cartTotal });
 
+  cart.push(product);
+  res.json({ message: 'Product added to cart', cart, cartTotal: calculateTotal() });
 });
 
-// Calculate the price based on productId
-function calculateCartTotal() {
+function calculateTotal() {
   return cart.reduce((total, product) => total + product.price, 0);
-}
-
-function getProductPrice(productId) {
-  if (productId === '1') {
-    return 150; // Product 1 price
-  } else if (productId === '2') {
-    return 200; // Product 2 price
-  } else {
-    return 0; // Default price if productId is not recognized
-  }
-}
-
-// Define a function to get the product name based on the productId
-function getProductName(productId) {
-  if (productId === '1') {
-    return 'Cotton Comforter';
-  } else if (productId === '2') {
-    return 'Linen Cover';
-  } else {
-    return 'Unknown Product';
-  }
 }
 
 app.get('/api/cart', (req, res) => {
@@ -52,16 +37,14 @@ app.get('/api/cart', (req, res) => {
 });
 
 app.post('/api/checkout', (req, res) => {
-  const cartItems = cart.slice();
-  const total = calculateTotal(cartItems);
+  if (cart.length === 0) {
+    return res.status(400).json({ message: 'Cart is empty' });
+  }
+
+  const total = calculateTotal();
   cart.length = 0;
   res.json({ message: 'Checkout successful', orderTotal: total });
 });
-
-// Calculate the total cost of items in the cart
-function calculateTotal(cartItems) {
-  return cartItems.reduce((total, item) => total + item.price, 0);
-}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -69,11 +52,6 @@ app.listen(PORT, () => {
 });
 
 // Define a default route to serve index.html
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Serve the server-side script only when requested by the server
-app.get('/server.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'server.js'));
 });
